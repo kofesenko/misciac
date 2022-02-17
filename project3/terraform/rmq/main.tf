c441511fd4ac09c52003ded4fbe7925b33c3ae61
kofesenko <kofesenko@gmail.com>
2022-02-17 11:02:41 -0800

    ansible

--- project3/terraform/rmq/main.tf
+++ project3/terraform/rmq/main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
   region = "eu-west-1"
-  
+
 }
 
 resource "aws_instance" "rmq" {
     ami = "ami-07d8796a2b0f8d29c"
     instance_type = "t2.micro"
-    key_name = "mydevops2"
+    key_name = "rabbitmq"
     vpc_security_group_ids = [ "sg-0b5d18ac4ab5774e0" ]
 
     tags = {
