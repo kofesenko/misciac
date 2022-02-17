bfe32933c45bd8a888bd1f6b9d81180702cc1b91
kofesenko <kofesenko@gmail.com>
2022-02-17 06:49:34 -0800

    project3 - terraform

---
+++ project3/terraform/rmq/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "eu-west-1"
+  profile = var.profile
+}
+
+resource "aws_instance" "rmq" {
+    ami = "ami-07d8796a2b0f8d29c"
+    instance_type = "t2.micro"
+    key_name = "mydevops"
+    vpc_security_group_ids = [ "sg-0b5d18ac4ab5774e0" ]
+
+    tags = {
+        Name = var.name
+        Group = var.group
+
+    }
+}
---
+++ project3/terraform/rmq/variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+    description = "Name the instance on deploy"
+}
+
+variable "group" {
+    description = "The group tag for ansible to identify RMQ"
+}
+
+variable "profile" {
+        description = "profile that we will use for deploy"
+}
