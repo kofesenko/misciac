ddda171a696b2895e384e4685bc1356f30710b85
kofesenko <kofesenko@gmail.com>
2022-02-17 12:07:51 -0800

    RDS conf

---
+++ project4/infrastructure/rds/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    profile = var.profile
+    region = "eu-west-1"
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage = 20
+  storage_type = "gp2"
+  engine = "postgres"
+  instance_class = "db.t2.micro"
+  name = "products_db"
+  username = "postgres"
+  password = var.password
+  identifier = var.id
+  publicly_accessible = true
+# vpc_security_group_ids = [ "value" ] 
+}
