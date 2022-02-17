0843c5a4d7e6da9bc5a1cec7788556c23fc16934
kofesenko <kofesenko@gmail.com>
2022-02-17 08:33:13 -0800

    configuration changes

--- project3/terraform/rmq/main.tf
+++ project3/terraform/rmq/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.74.0"
-    }
-  }
-}
-
 provider "aws" {
   region = "eu-west-1"
   profile = var.profile
